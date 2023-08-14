9eed62987793bbf930342d3e5cda25d2e67bc5f3
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2023-08-14 05:15:59 +0300

    refactor: done with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import getData from "services/getData";
 import Button from "./button/Button";
 import ImageGallery from "./imageGallery/ImageGallery";
@@ -6,57 +6,51 @@ import Loader from "./loader/Loader";
 import Modal from "./modal/Modal";
 import Searchbar from "./searchbar/Searchbar";
 
-export class App extends Component {
-  state = {
-    images: [],
-    showLoader: true,
-    currentPage: 1,
-    searchBar: "",
-    totalHits: '',
-    selectedImage: null,
-  };
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [showLoader, setShowLoader] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchBar, setSearchBar] = useState("");
+  const [totalHits, setTotalHits] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
 
-  searchSubmit = async (text) => {
-    if (text === this.state.searchBar) {
-      return
+  const searchSubmit = (text) => {
+    if (text === searchBar) {
+      return;
     }
-    this.setState({
-      searchBar: text,
-      images: [],
-      currentPage: 1,
-    });
+    setSearchBar(text);
+    setImages([]);
+    setCurrentPage(1);
   };
 
-  loadMore = () => {
-    this.setState((prevState) => ({
-      currentPage: prevState.currentPage + 1,
-    }));
+  const loadMore = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
   };
 
-  handleImageClick = (largeImageURL) => {
-    this.setState({ selectedImage: largeImageURL });
-    window.addEventListener("keydown", this.handleEsc);
+  const handleImageClick = (largeImageURL) => {
+    setSelectedImage(largeImageURL);
+    window.addEventListener("keydown", handleEsc);
   };
 
-  handleCloseModal = () => {
-    this.setState({ selectedImage: null });
-    window.removeEventListener("keydown", this.handleEsc);
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+    window.removeEventListener("keydown", handleEsc);
   };
 
-  handleEsc = (event) => {
+  const handleEsc = (event) => {
     if (event.keyCode === 27) {
-      this.handleCloseModal();
+      handleCloseModal();
     }
   };
 
-  loadImages = () => {
-    this.setState({ showLoader: true });
+  const loadImages = () => {
+    setShowLoader(true);
     const imageElements = document.querySelectorAll("img");
     let imagesLoaded = 0;
     const handleImageLoad = () => {
       imagesLoaded++;
       if (imagesLoaded === imageElements.length) {
-        this.setState({ showLoader: false });
+        setShowLoader(false);
       }
     };
 
@@ -69,80 +63,65 @@ export class App extends Component {
     });
   };
 
-  handleSearch = async () => {
-    this.setState({ showLoader: true });
-    const response = await getData(this.state.searchBar);
+  const handleSearch = async () => {
+    setShowLoader(true);
+    const response = await getData(searchBar);
     if (response?.totalHits > 0) {
-      this.setState(
-        {
-          images: response.hits,
-          totalHits: response.totalHits,
-        },
-        () => {
-          this.loadImages();
-        }
-      );
-    }
-    else {
-      alert('nothing found')
-      this.setState({ showLoader: false });
+      setImages(response.hits);
+      setTotalHits(response.totalHits);
+    } else {
+      alert('Nothing found');
+      setShowLoader(false);
     }
   };
 
-  handleLoadMoreImages = async () => {
-    const response = await getData(
-      this.state.searchBar,
-      this.state.currentPage
-    );
+  const handleLoadMoreImages = async () => {
+    const response = await getData(searchBar, currentPage);
     if (response?.totalHits) {
-      this.setState(
-        (prevState) => ({
-          images: [...prevState.images, ...response.hits],
-        }),
-        () => {
-          this.loadImages();
-        }
-      );
+      setImages((prevImages) => [...prevImages, ...response.hits]);
+      loadImages();
     }
-  }
+  };
 
-  componentDidMount() {
-    this.searchSubmit("car");
-  }
+  useEffect(() => {
+    searchSubmit("car");
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.searchBar !== prevState.searchBar) {
-      this.handleSearch();
+  useEffect(() => {
+    if (searchBar !== "") {
+      handleSearch();
     }
-    if (this.state.currentPage !== prevState.currentPage && this.state.currentPage !== 1) {
-      this.handleLoadMoreImages();
-    }
-  }
+  }, [searchBar]);
 
-  render() {
-    const { images, showLoader, totalHits, selectedImage } = this.state;
-    return (
-      <div className="App">
-        <header>
-          <Searchbar onSearchSubmit={this.searchSubmit} />
-        </header>
-        <main>
-          {showLoader && <Loader />}
-          <>
-            <ImageGallery images={images} handleImageClick={this.handleImageClick} />
-            {images.length < totalHits && !showLoader && images.length > 0 && <Button loadMore={this.loadMore} />}
-          </>
-        </main>
-        {selectedImage && (
-          <Modal
-            imageUrl={selectedImage}
-            altText="Selected Image"
-            handleCloseModal={this.handleCloseModal}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    if (currentPage !== 1) {
+      handleLoadMoreImages();
+    }
+  }, [currentPage]);
+
+  useEffect(() => loadImages(), [images])
+
+  return (
+    <div className="App">
+      <header>
+        <Searchbar onSearchSubmit={searchSubmit} />
+      </header>
+      <main>
+        {showLoader && <Loader />}
+        <>
+          <ImageGallery images={images} handleImageClick={handleImageClick} />
+          {images.length < totalHits && !showLoader && images.length > 0 && <Button loadMore={loadMore} />}
+        </>
+      </main>
+      {selectedImage && (
+        <Modal
+          imageUrl={selectedImage}
+          altText="Selected Image"
+          handleCloseModal={handleCloseModal}
+        />
+      )}
+    </div>
+  );
+};
 
 export default App;
--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,45 +1,36 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import styles from './Searchbar.module.css'
 
-class Searchbar extends Component {
-    state = {
-        searchBar: "",
+const Searchbar = ({ onSearchSubmit }) => {
+    const [searchBar, setSearchBar] = useState("");
+
+    const handleInputChange = (e) => {
+        setSearchBar(e.target.value);
     };
 
-    handleInputChange = (e) => {
-        this.setState({ searchBar: e.target.value });
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        onSearchSubmit(searchBar);
     };
 
-    handleSearchSubmit = () => {
-        this.props.onSearchSubmit(this.state.searchBar);
-    };
-
-    render() {
-        return (
-            <div className={styles.Searchbar}>
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    this.handleSearchSubmit();
-                }
-                } className={styles.SearchForm}>
-                    <button type="submit" className={styles.SearchFormButton} >Search</button>
-                    <input
-                        className={styles.SearchFormInput}
-                        type="text"
-                        value={this.state.searchBar}
-                        onChange={this.handleInputChange}
-                    />
-                </form>
-
-            </div >
-        );
-    }
+    return (
+        <div className={styles.Searchbar}>
+            <form onSubmit={handleFormSubmit} className={styles.SearchForm}>
+                <button type="submit" className={styles.SearchFormButton}>Search</button>
+                <input
+                    className={styles.SearchFormInput}
+                    type="text"
+                    value={searchBar}
+                    onChange={handleInputChange}
+                />
+            </form>
+        </div>
+    );
 }
 
 Searchbar.propTypes = {
     onSearchSubmit: PropTypes.func.isRequired
 }
-
 
 export default Searchbar;
